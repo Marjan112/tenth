8fe2227cf4ed982411cfcdf2d2abe6d86a3f4df8
Marjan112 <marjanramadanovic1234@gmail.com>
2023-09-26 21:30:03 +0200

    #

--- src/include/compiler.hpp
+++ src/include/compiler.hpp
@@ -2,10 +2,11 @@
 #define COMPILER_HPP
 
 #include "opcodes.hpp"
+#include "print.hpp"
 
 namespace tenth {
     void compile_program(const program_t& program) {
-        
+        UNIMPLEMENTED("Compiling is not implemented yet.");
     }
 }
 
