ae4cfa9230b873319d77ced640235a4dc5dcde39
Marjan112 <marjanramadanovic1234@gmail.com>
2023-09-26 21:36:02 +0200

    #

--- src/include/compiler.hpp
+++ src/include/compiler.hpp
@@ -1,12 +1,16 @@
 #ifndef COMPILER_HPP
 #define COMPILER_HPP
 
+#include <fstream>
+
 #include "opcodes.hpp"
 #include "print.hpp"
 
 namespace tenth {
-    void compile_program(const program_t& program) {
+    void compile_program(std::string filename, const program_t& program) {
         UNIMPLEMENTED("Compiling is not implemented yet.");
+
+        std::ofstream output_file(filename);    
     }
 }
 
